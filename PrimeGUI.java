aad42944a3ae364106763eccb9548be85292fc71
Nachum Schwartz <nschwartz814@gmail.com>
2022-11-15 18:24:50 -0500

    Begin GUI code

---
+++ PrimeGUI.java
@@ -0,0 +1,7 @@
+import javax.swing.JFrame;
+
+public class PrimeGUI extends JFrame{
+    public static void main(String[] args){
+        PrimeGUI obj = new PrimeGUI();
+    }//end main method  
+}//end class 
