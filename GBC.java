da8f8004696f3f42defac57968ad059103a6f506
Nachum Schwartz <nschwartz814@gmail.com>
2022-11-16 17:51:56 -0500

    New Grab Bag Constraints File

---
+++ GBC.java
@@ -0,0 +1,43 @@
+/**
+* This class simplifies the use of the GridBagConstraints class. 
+* @version 1.01 2004-05-06
+* @author Cay Horstmann
+*
+*/
+
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+    
+    public class GBC extends GridBagConstraints{
+        public GBC(int gridx, int gridy){
+            this.gridx = gridx;
+            this.gridy = gridy;
+        }
+        public GBC(int gridx, int gridy, int gridwidth, int gridheight){
+            this.gridx = gridx;
+            this.gridy = gridy;
+            this.gridwidth = gridwidth;
+            this.gridheight = gridheight;
+        }
+        public GBC setWeight(double weightx, double weighty){
+            this.weightx = weightx;
+            this.weighty = weighty;
+            return this;
+        }
+        public GBC setFill(int fill){
+            this.fill = fill;
+            return this;
+        }
+        public GBC setAnchor(int anchor){
+            this.anchor = anchor;
+            return this;
+        }
+        public GBC setInsets(int distance){
+            this.insets = new Insets(distance, distance, distance, distance);
+            return this;
+        }
+        public GBC setInsets(int top, int left, int bottom, int right){
+            this.insets = new Insets(top, left, bottom, right);
+            return this;
+        }
+    } 
